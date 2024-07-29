eff90de3a7a06bd25bcd2bf8ea99b7db5c8025e3
DaniAlej <168309704+daniAlej@users.noreply.github.com>
2024-07-29 10:43:13 -0500

    migracion

---
+++ GestorTickets/Migrations/202407290209178_InitialCreate.cs
@@ -0,0 +1,63 @@
+﻿namespace GestorTickets.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Comidas",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Nombre = c.String(),
+                        Precio = c.Decimal(nullable: false, precision: 18, scale: 2),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.HistorialTickets",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UsuarioId = c.Int(nullable: false),
+                        Accion = c.String(),
+                        Cantidad = c.Int(nullable: false),
+                        Fecha = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Tickets",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UsuarioId = c.Int(nullable: false),
+                        Cantidad = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Usuarios",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        NombreUsuario = c.String(),
+                        Contraseña = c.String(),
+                        Rol = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Usuarios");
+            DropTable("dbo.Tickets");
+            DropTable("dbo.HistorialTickets");
+            DropTable("dbo.Comidas");
+        }
+    }
+}
---
+++ GestorTickets/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace GestorTickets.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<GestorTickets.DAL.GestorTicket>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "GestorTickets.DAL.GestorTicket";
+        }
+
+        protected override void Seed(GestorTickets.DAL.GestorTicket context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
